1e5acd1869042c4b64d6ec7c509367f7eb807350
gabbydgab <gamba@gabbydgab.com>
2016-05-18 14:16:58 +0800

    added error and partials

--- config/autoload/templates.config.php
+++ config/autoload/templates.config.php
@@ -16,9 +16,22 @@ return [
     'templates'     => [
         'layout'    => 'dashboard/adminlte',
         'map'       => [
-            'dashboard/adminlte'    => __DIR__ . '/../../templates/adminlte/layout/version2.phtml',
+            'dashboard/adminlte'                => __DIR__ . '/../../templates/adminlte/layout/version2.phtml',
+            'error/error'                       => __DIR__ . '/../../templates/error/error.phtml',
+            'error/404'                         => __DIR__ . '/../../templates/error/404.phtml',
+            'template/footer'                   => __DIR__ . '/../../templates/layout/partial/footer.phtml',
+            'template/navigation/sidebar'       =>
+                __DIR__ . '/../../templates/layout/template/navigation/sidebar.phtml',
+            'template/navigation/header'        => __DIR__ . '/../../templates/layout/template/navigation/header.phtml',
+            'template/navigation/breadcrumbs'   =>
+                __DIR__ . '/../../templates/layout/template/navigation/breadcrumbs.phtml',
+            'partial/navigation/header'         => __DIR__ . '/../../templates/layout/partial/navigation/header.phtml',
+            'partial/navigation/sidebar'        => __DIR__ . '/../../templates/layout/partial/navigation/sidebar.phtml',
+            'partial/navigation/breadcrumbs'    =>
+                __DIR__ . '/../../templates/layout/partial/navigation/breadcrumbs.phtml',
         ],
-        'paths'      => [            
+        'paths'      => [
+            'error'     => [__DIR__ . '/../../templates/error'],
             'widget'    => [__DIR__ . '/../../templates/adminlte/widget'],
             'page'      => [__DIR__ . '/../../templates/adminlte/page']
         ]
@@ -26,6 +39,6 @@ return [
     'view_helpers'  => [
         'factories'     => [],
         'invokables'    => [],
-        'aliases'       => []       
+        'aliases'       => []
     ]
 ];
